3aa90488b1a6d388907228ce49a0927d2a5ea0a3
nebelgrau77 <misiek@gmx.it>
2025-11-15 21:35:26 +0100

    fixed spawner name in main

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
 {% if hal == "embassy" -%}
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_25, Level::Low);
 
